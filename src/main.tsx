961864dd3c7b12b1f01c500fb02ff7d09781331a
DukeSpontaneous <eai_bat@tut.by>
2023-07-05 07:10:57 +0300

    Первичное проецирование.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import Header from "./Header.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+root.render(
   <React.StrictMode>
+    <Header />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
